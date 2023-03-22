8b6d57bba2405f7275e223d6f8b7a4da3e4c183a
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2023-03-22 17:08:45 -0300

    Add: register screen and render tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,30 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renderiza tudo sem crash', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+describe('Render Test', () => {
+    it('renders name without crashing', ()=> {
+        render(<App />)
+        expect(screen.getByTestId('nome')).toBeInTheDocument()
+    })
+    it('renders email without crashing', ()=> {
+        render(<App />)
+        expect(screen.getByTestId('email')).toBeInTheDocument()
+    })
+    it('renders senha without crashing', ()=> {
+        render(<App />)
+        expect(screen.getByTestId('senha')).toBeInTheDocument()
+    })
+    it('renders confirmasenha without crashing', ()=> {
+        render(<App />)
+        expect(screen.getByTestId('confirmasenha')).toBeInTheDocument()
+    })
+    it('renders botaoregistrar without crashing', ()=> {
+        render(<App />)
+        expect(screen.getByTestId('botaoregistrar')).toBeInTheDocument()
+    })
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [dadosUsuario, setDadosUsuario] = useState({ 
+      nome: '', 
+      email: '', 
+      senha: '', 
+      confirmaSenha: '' 
+    });
+
+    function handleSubmit(event: any){
+      event.preventDefault()
+      console.log(dadosUsuario)
+
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label htmlFor="nome">Nome:
+        <input type="text" id="nome" name="nome" data-testid="nome"
+          value={dadosUsuario.nome}
+          onChange={e => setDadosUsuario({ ...dadosUsuario, nome: e.target.value })}/>
+        </label>
+        {dadosUsuario.nome}
+        <br />
+
+        
+
+        <label htmlFor="email">E-mail:
+        <input type="email" id="email" name="email" data-testid="email"
+           value={dadosUsuario.email}
+           onChange={e => setDadosUsuario({ ...dadosUsuario, email: e.target.value })}/>
+        </label>
+        {dadosUsuario.email}
+        <br />
+
+        <label htmlFor="senha">Senha:
+        <input type="password" id="senha" name="senha" data-testid="senha"
+           value={dadosUsuario.senha}
+           onChange={e => setDadosUsuario({ ...dadosUsuario, senha: e.target.value })}/>
+        </label>
+        {dadosUsuario.senha}
+        <br />
+
+        <label htmlFor="confirmasenha">Confirme a senha:
+        <input type="password" id="confirmasenha" name="confirmasenha" data-testid="confirmasenha"
+           value={dadosUsuario.confirmaSenha}
+           onChange={e => setDadosUsuario({ ...dadosUsuario, confirmaSenha: e.target.value })}/>
+        </label>
+        {dadosUsuario.confirmaSenha}
+        <br />
+
+        <button type="submit" data-testid="botaoregistrar" onClick={handleSubmit}>Registrar</button>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
